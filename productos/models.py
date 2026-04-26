d87a566710c4d0cdc8ae5f848e2360031f23aa7b
DannyAS <dannyalfaros45@gmail.com>
2026-04-26 01:45:42 -0600

    Modelo productos

--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    nombre = models.CharField(verbose_name='Nombre', max_length=100)
+    precio = models.FloatField(verbose_name='Precio', max_length=32)
+    existencias = models.IntegerField(verbose_name='Existencias')
+    def __str__(self):
+        return self.nombre
