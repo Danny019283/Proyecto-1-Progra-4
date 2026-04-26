69fc314c0c945ef955df88d21317d903f0fcdf36
DannyAS <dannyalfaros45@gmail.com>
2026-04-26 01:46:10 -0600

    Modelo productos creado y conexion con bd

--- server/settings.py
+++ server/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import pymysql
+pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,8 +79,12 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Proyecto_CRUD',
+        'USER': 'admin',
+        'PASSWORD': 'Server1234',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
